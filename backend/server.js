9f66fd5f0a6563bc713cfec54fe21aeb48004b5d
DjKesu <66878328+DjKesu@users.noreply.github.com>
2021-04-22 13:10:42 +0530

    Backend created. Basic frontend completed. Functions now.

--- backend/server.js
+++ backend/server.js
@@ -2,16 +2,14 @@ const express = require('express');
 const cors = require ('cors');
 const mongoose = require('mongoose');
 
-require('dotenv').config();
-
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
-const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
+const uri = process.env.ATLAS_URI || 'mongodb+srv://admin:Rhpu679326XEWY2@cluster0.ufot7.mongodb.net/test?retryWrites=true';
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
 
 const connection = mongoose.connection;
 connection.once('open', () => {
@@ -27,3 +25,7 @@ app.use('/users', usersRouter);
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
 });
+
+new Promise((resolve, reject) => {
+    setTimeout(() => reject('woops'), 1000);
+  });
---
+++ src/components/edit-exercise-list.component.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+export default class EditExercise extends Component{
+    render() {
+        return(
+            <div>
+                <p>Things are working around here! - EditExercise</p>
+            </div>
+        )}
+}
