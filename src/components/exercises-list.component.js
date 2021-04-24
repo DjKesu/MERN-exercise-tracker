a3ff799b14b3cf3856a6dffc49ccb24c44af3100
DjKesu <km67000@yahoo.com>
2021-04-24 15:15:00 +0530

    Fixed minor issues

--- src/components/exercises-list.component.js
+++ src/components/exercises-list.component.js
@@ -1,51 +1,51 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import axios from 'axios';
-import Exercise from '../../backend/models/exercise.model';
+// import axios from 'axios';
+// import Exercise from '../../backend/models/exercise.model';
 
-const Exercise = (props) => {
-    <tr>
-        <td>{props.exercise.username}</td>
-        <td>{props.exercise.description}</td>
-        <td>{props.exercise.duration}</td>
-        <td>{props.exercise.date.substring(0,10)}</td>
-        <td>
-            <Link to={"/edit/"+exercise._id}>Edit</Link> | <a href="#" onClick = {() => {props.deleteExercise(props.exercise._id)}}>Delete</a>
-        </td>
-    </tr>
-}
+// const Exercise = (props) => {
+//     <tr>
+//         <td>{props.exercise.username}</td>
+//         <td>{props.exercise.description}</td>
+//         <td>{props.exercise.duration}</td>
+//         <td>{props.exercise.date.substring(0,10)}</td>
+//         <td>
+//             <Link to={"/edit/"+exercise._id}>Edit</Link> | <a href="#" onClick = {() => {props.deleteExercise(props.exercise._id)}}>Delete</a>
+//         </td>
+//     </tr>
+// }
 
 
 export default class ExercisesList extends Component{
-    constructor(props){
-        super(props);
+    // constructor(props){
+    //     super(props);
 
-        this.deleteExercise = this.deleteExercise.bind(this);
+    //     this.deleteExercise = this.deleteExercise.bind(this);
         
-        this.state = { exercises: []};
-    }
+    //     this.state = { exercises: []};
+    // }
 
-    componentDidMount(){
-        axios.get('http://localhost:3000/exercises/')
-            .then( response =>{
-                this.setState({exercises: response.data})
-            })
-            .catch((error) => {console.log(error)});
-    }
+    // componentDidMount(){
+    //     axios.get('http://localhost:3000/exercises/')
+    //         .then( response =>{
+    //             this.setState({exercises: response.data})
+    //         })
+    //         .catch((error) => {console.log(error)});
+    // }
 
-    deleteExercise(id) {
-        axios.delete('http://localhost:3000/exercises'+id)
-            .then( res => console.log(res.data));
-        this.setState({
-            exercises: this.state.exercises.filter( el => el._id !== id)
-        })
-    }
+    // deleteExercise(id) {
+    //     axios.delete('http://localhost:3000/exercises'+id)
+    //         .then( res => console.log(res.data));
+    //     this.setState({
+    //         exercises: this.state.exercises.filter( el => el._id !== id)
+    //     })
+    // }
 
-    exercisesList() {
-        return this.state.exercises.map(currentexercise => {
-            return <Exercises exercise = {currentexercise} deleteExercise= { this.deleteExercise} key={ this.currentexercise._id} />
-        })
-    }
+    // exercisesList() {
+    //     return this.state.exercises.map(currentexercise => {
+    //         return <Exercises exercise = {currentexercise} deleteExercise= { this.deleteExercise} key={ this.currentexercise._id} />
+    //     })
+    // }
 
     render() {
         return(
@@ -61,9 +61,9 @@ export default class ExercisesList extends Component{
                            <th>Actions</th>
                        </tr>
                    </thead>
-                   <tbody>
+                   {/* <tbody>
                        { this.exercisesList() }
-                   </tbody>
+                   </tbody> */}
                </table>
             </div>
         )}
